26f38dd786dbc24637272a38e5591ca8f4ae44e7
mrMixixr9k <mrminixr9k@gmail.com>
2024-05-30 20:19:26 +0300

    Update cactusWeb.py

--- cactusWeb.py
+++ cactusWeb.py
@@ -3,6 +3,9 @@ import flet as ft
 def main(page: ft.Page):
     page.theme_mode = 'dark'
     page.title = 'Cactus Clicker'
+    btn = ft.TextButton(text="Button")
+    page.add(btn)
+    page.update()
 
 if __name__ == "__main__":
-    ft.app(target=main, view=None, port=80)
+    ft.app(target=main, view=ft.WEB_BROWSER, port=80)
