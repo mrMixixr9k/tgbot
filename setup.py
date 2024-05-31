a395b9ac55a2db16c392ca20ada0df01c577c238
mrMixixr9k <mrminixr9k@gmail.com>
2024-05-31 08:57:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-pip install -r requirements.txt
+from setuptools import setup, find_packages
+
+setup(
+    name='tgbot',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'flet',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tgbot = tgbot.main:main',
+        ],
+    },
+)
